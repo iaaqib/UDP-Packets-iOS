ea38993e00b5cea8c7e4a3aa2deb1cb2173a1367
Aaqib Hussain <iaaqibhussain@outlook.com>
2017-08-26 22:43:04 +0500

    Initial Commit

---
+++ RemoteControl-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteControl-iOS
+//
+//  Created by Aaqib Hussain on 26/8/17.
+//  Copyright © 2017 Aaqib Hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
